2768f202b023f1f40454cea6d597967df5bc5107
gdulus <gdula.pawel@gmail.com>
2014-06-24 15:08:15 +0200

    Update Application.groovy

--- src/main/groovy/com/ps/service/Application.groovy
+++ src/main/groovy/com/ps/service/Application.groovy
@@ -1,7 +1,5 @@
 package com.ps.service
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
@@ -12,9 +10,6 @@ import org.springframework.context.annotation.ComponentScan
 @ComponentScan
 @EnableAutoConfiguration
 class Application {
-
-    @Autowired
-    RabbitTemplate rabbitTemplate;
 
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
